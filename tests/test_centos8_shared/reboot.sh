4ad6da7650f9c41c27bb087e031dd122d9ee0fdd
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2021-03-20 11:51:40 +0100

    fix incron idempotency - closes #211
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>
    Change-Id: I929838e47875208e584cc87e7c194435bce0ded6
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>

--- tests/test_centos8_shared/reboot.sh
+++ tests/test_centos8_shared/reboot.sh
@@ -1,2 +1,2 @@
-ansible -i libvirt-inventory.py consul_server,consul_client -b  -m reboot -a "reboot_timeout=300"
-ansible -i libvirt-inventory.py consul_server,consul_client -m wait_for -a "port=22 timeout=300"
+ANSIBLE_CONFIG=fabsible.cfg ansible -i libvirt-inventory.py consul_server,consul_client -b  -m reboot -a "reboot_timeout=300"
+ANSIBLE_CONFIG=fabsible.cfg ansible -i libvirt-inventory.py consul_server,consul_client -m wait_for -a "port=22 timeout=300"
